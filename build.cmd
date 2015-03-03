cab4bb313e833751f5777b87a2ae6e66a9e3eedd
SkyLined <berendjanwever@gmail.com>
2015-03-03 11:08:53 +0100

    Update package.json
    * Add keywords
    * Add bugs url
    * Restrict to installation on ia32 and x64 versions of Windows.
    * Remove automatic build on npm install (the module had pre-compiled
    binaries, so there is no need for a build).

--- build.cmd
+++ build.cmd
@@ -1,24 +1,26 @@
 @ECHO OFF
-:: CLEANUP
-
-:: Walk through build-targets.txt and build each target
-FOR /F "eol=# tokens=1,2" %%I in (build-targets.txt) DO (
-  FOR /F "delims=v. tokens=1,2" %%K IN ("%%J") DO (
-    ECHO == Building for %%I architecture and node v%%K.%%L.*
-    CALL node-gyp install rebuild --arch=%%I --target=%%J
-    IF ERRORLEVEL 1 (
-      ECHO Build for %%I architecture and node v%%K.%%L.* failed.
-      EXIT /B %ERRORLEVEL%
+IF "%~1" == "install" (
+  ECHO This module comes with pre-compiled binaries. To rebuild them run "build.cmd".
+) ELSE (
+  :: Walk through build-targets.txt and build each target
+  FOR /F "eol=# tokens=1,2" %%I in (build-targets.txt) DO (
+    FOR /F "delims=v. tokens=1,2" %%K IN ("%%J") DO (
+      ECHO == Building for %%I architecture and node v%%K.%%L.*
+      CALL node-gyp install rebuild --arch=%%I --target=%%J
+      IF ERRORLEVEL 1 (
+        ECHO Build for %%I architecture and node v%%K.%%L.* failed.
+        EXIT /B %ERRORLEVEL%
+      )
+      :: Copy binary to sub-folder
+      IF NOT EXIST "bin\%%I\%%K.%%L" MKDIR "bin\%%I\%%K.%%L"
+      COPY "build\Release\*.node" "bin\%%I\%%K.%%L\" 2>&1 >nul
+      IF ERRORLEVEL 1 (
+        ECHO Cannot copy build for %%I architecture and node v%%K.%%L.* to "bin\%%I\%%K.%%L\" folder.
+        EXIT /B %ERRORLEVEL%
+      )
+      :: Delete obsolete build folder
+      CALL node-gyp clean
     )
-    :: Copy binary to sub-folder
-    IF NOT EXIST "bin\%%I\%%K.%%L" MKDIR "bin\%%I\%%K.%%L"
-    COPY "build\Release\*.node" "bin\%%I\%%K.%%L\" 2>&1 >nul
-    IF ERRORLEVEL 1 (
-      ECHO Cannot copy build for %%I architecture and node v%%K.%%L.* to "bin\%%I\%%K.%%L\" folder.
-      EXIT /B %ERRORLEVEL%
-    )
-    :: Delete obsolete build folder
-    CALL node-gyp clean
   )
 )
 EXIT /B 0
