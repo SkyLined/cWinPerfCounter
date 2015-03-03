9d280292d6d6a79d32cd26807baecbca4ac8f4b4
SkyLined <berendjanwever@gmail.com>
2015-03-03 20:11:46 +0100

    Build binaries with full version number
    Binaries are now stored in a folder containing the full node.js version
    number, which allows for greater compatibility when breaking changes are
    introduced in minor version updates. It also simplifies the code in
    build.cmd

--- build.cmd
+++ build.cmd
@@ -4,23 +4,21 @@ IF "%~1" == "install" (
 ) ELSE (
   :: Walk through build-targets.txt and build each target
   FOR /F "eol=# tokens=1,2" %%I in (build-targets.txt) DO (
-    FOR /F "delims=v. tokens=1,2" %%K IN ("%%J") DO (
-      ECHO == Building for %%I architecture and node v%%K.%%L.*
-      CALL node-gyp install rebuild --arch=%%I --target=%%J
-      IF ERRORLEVEL 1 (
-        ECHO Build for %%I architecture and node v%%K.%%L.* failed.
-        EXIT /B %ERRORLEVEL%
-      )
-      :: Copy binary to sub-folder
-      IF NOT EXIST "bin\%%I\%%K.%%L" MKDIR "bin\%%I\%%K.%%L"
-      COPY "build\Release\*.node" "bin\%%I\%%K.%%L\" 2>&1 >nul
-      IF ERRORLEVEL 1 (
-        ECHO Cannot copy build for %%I architecture and node v%%K.%%L.* to "bin\%%I\%%K.%%L\" folder.
-        EXIT /B %ERRORLEVEL%
-      )
-      :: Delete obsolete build folder
-      CALL node-gyp clean
+    ECHO == Building for %%I architecture and node %%J
+    CALL node-gyp install rebuild --arch=%%I --target=%%J
+    IF ERRORLEVEL 1 (
+      ECHO Build for %%I architecture and node v%%J failed.
+      EXIT /B %ERRORLEVEL%
     )
+    :: Copy binary to sub-folder
+    IF NOT EXIST "bin\%%I\%%J" MKDIR "bin\%%I\%%J"
+    COPY "build\Release\*.node" "bin\%%I\%%J\" 2>&1 >nul
+    IF ERRORLEVEL 1 (
+      ECHO Cannot copy build to "bin\%%I\%%J\" folder.
+      EXIT /B %ERRORLEVEL%
+    )
+    :: Delete obsolete build folder
+    CALL node-gyp clean
   )
 )
 EXIT /B 0
