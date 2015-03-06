22d05717e677f9ca873da74afbdc9ac2696bff72
SkyLined <berendjanwever@gmail.com>
2015-03-06 10:35:40 +0100

    Make publish.cmd handle errors

--- publish.cmd
+++ publish.cmd
@@ -1,4 +1,25 @@
 @ECHO OFF
-CALL npm version patch -m "Published version %%%%s"
-CALL npm publish
-IF ERRORLEVEL 1 PAUSE
+:START
+  ECHO === Updating version number...
+  CALL npm version patch -m "Version updated to %%%%s"
+  IF ERRORLEVEL 1 (
+    ECHO === Cannot update version number!
+    GOTO :ERROR
+  )
+  CALL npm publish
+  IF ERRORLEVEL 1 (
+    ECHO === Cannot publish package!
+    ECHO === Reverting version update commit...
+    git reset --hard HEAD~1
+    IF ERRORLEVEL 1 (
+      ECHO === Cannot revert commit to update version number!
+    )
+    GOTO :ERROR
+  )
+  EXIT /B 0
+
+:ERROR
+  ECHO Press CTRL+C to terminate or ENTER to retry...
+  PAUSE >nul
+  GOTO :START
+
